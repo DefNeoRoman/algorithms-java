bffbb7bf2593cd0e709ef96c6ab244dbeedcd6fa
dale.seo <dale.seo@gmail.com>
2015-08-05 22:19:25 +0900

    ProductOfAllOtherNumbers

--- src/interviewcake/ProductOfAllOtherNumbers.java
+++ src/interviewcake/ProductOfAllOtherNumbers.java
@@ -1,5 +1,6 @@
 package interviewcake;
 
+
 /**
 You have an array of integers, and for each index you want to find the product of every integer except the integer at that index.
 Write a function get_products_of_all_ints_except_at_index() that takes an array of integers and returns an array of the products.
@@ -17,7 +18,10 @@ Do not use division in your solution.
 */
 public class ProductOfAllOtherNumbers {
 	
-	int[] getProductsOfAllIntsExceptAtIndex(int[] numbers) {
+	/**
+	 * O(n2)
+	 */
+	public int[] getProductsOfAllIntsExceptAtIndex1(int[] numbers) {
 		int[] products = new int[numbers.length];
 		for (int i = 0; i < numbers.length; i++) {
 			int product = 1;
@@ -27,6 +31,31 @@ public class ProductOfAllOtherNumbers {
 			}
 			products[i] = product;
 		}
+		return products;
+	}
+	
+	/**
+	 * O(n)
+<pre>
+To find the products of all the integers except the integer at each index, we'll go through our array greedily twice. First we get the products of all the integers before each index, and then we go backwards to get the products of all the integers after each index.
+When we multiply all the products before and after each index, we get our answer—the products of all the integers except the integer at each index! 
+</pre>
+	 */
+	public int[] getProductsOfAllIntsExceptAtIndex2(int[] numbers) {
+		int[] products = new int[numbers.length];
+		
+		int productBefore = 1;
+		for (int i = 0; i < numbers.length; i++) {
+			products[i] = productBefore;
+			productBefore *= numbers[i];
+		}
+		
+		int productAfter = 1;
+		for (int i = numbers.length - 1; i >= 0; i--) {
+			products[i] *= productAfter;
+			productAfter *= numbers[i];
+		}
+		
 		return products;
 	}
 
