9cae121994a90be13b887ab5fe49081ba11dbe09
dale.seo <dale.seo@gmail.com>
2015-08-12 22:11:30 +0900

    MakingChange

---
+++ src/interviewcake/MakingChange.java
@@ -0,0 +1,26 @@
+package interviewcake;
+
+/**
+ * https://www.interviewcake.com/question/coin
+ 
+ Write a function that, given:
+
+an amount of money
+a list of coin denominations
+computes the number of ways to make amount of money with coins of the available denominations.
+
+Example: for amount=4 (4¢) and denominations=[1,2,3] (1¢, 2¢ and 3¢), your program would output 4—the number of ways to make 4¢ with those denominations:
+
+1¢, 1¢, 1¢, 1¢
+1¢, 1¢, 2¢
+1¢, 3¢
+2¢, 2¢
+ 
+ */
+public class MakingChange {
+	
+	public int compute(int amont, int[] denominations) {
+		return 0;
+	}
+	
+}
