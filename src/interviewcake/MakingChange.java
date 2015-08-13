82c59b94918945d5bfa0a69bcf8ceae457334e19
dale.seo <dale.seo@gmail.com>
2015-08-13 22:35:14 +0900

    MakingChange 2nd try

--- src/interviewcake/MakingChange.java
+++ src/interviewcake/MakingChange.java
@@ -1,7 +1,11 @@
 package interviewcake;
 
+import java.util.Arrays;
+
 /**
  * https://www.interviewcake.com/question/coin
+ 
+이해가 잘 안 되서 해답보고 코딩 한 번 해보고 넘어감 ㅠㅠ 
  
  Write a function that, given:
 
@@ -19,8 +23,35 @@ Example: for amount=4 (4¢) and denominations=[1,2,3] (1¢, 2¢ and 3¢), your p
  */
 public class MakingChange {
 	
-	public int compute(int amont, int[] denominations) {
-		return 0;
+	public int compute(int amount, int[] denominations) {
+		System.out.printf(">> Checking ways to make %d with %s.\n", amount, Arrays.toString(denominations));
+		
+		if (amount == 0) {
+			System.out.println("Hit the amount.");
+			return 1;
+		}
+		
+		if (amount < 0) {
+			System.out.println("Overshot the amount.");
+			return 0;
+		}
+		
+		if (denominations.length == 0) {
+			System.out.println("Out of denominations.");
+			return 0;
+		}
+		
+		int currentCoin = denominations[0];
+		int[] restOfCoins = Arrays.copyOfRange(denominations, 1, denominations.length);
+		System.out.printf("currentCoin = %d / resetOfCoins = %s.\n", currentCoin, Arrays.toString(restOfCoins));
+		
+		int count = 0;
+		while (amount >= 0) {
+			count += compute(amount, restOfCoins);
+			amount -= currentCoin;
+		}
+		
+		return count;
 	}
 	
 }
