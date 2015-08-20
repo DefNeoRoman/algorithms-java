aa3569edfd97a92dc099616ea264cbbdc1115ae6
dale0713 <dale.seo@gmail.com>
2015-08-20 22:05:12 +0900

    MergeSortedArrays

---
+++ src/interviewcake/MergeSortedArrays.java
@@ -0,0 +1,16 @@
+package interviewcake;
+
+/**
+ * https://www.interviewcake.com/question/merge-sorted-arrays?utm_source=weekly_email
+ *
+Each order is represented by an "order id" (an integer).
+We have our lists of orders sorted numerically already, in arrays. Write a function to merge our arrays of orders into one sorted array.
+ */
+public class MergeSortedArrays {
+
+	public int[] mergeArrays(int[] firstArr, int[] secondArr) {
+		
+		return null;
+	}
+	
+}
