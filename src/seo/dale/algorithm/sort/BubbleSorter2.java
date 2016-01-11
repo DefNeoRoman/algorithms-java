3358018c0445b74d30bc2324683d9f3ac78d38b0
dale0713 <dale.seo@gmail.com>
2016-01-11 23:27:09 +0900

    BubbleSort

---
+++ src/seo/dale/algorithm/sort/BubbleSorter2.java
@@ -0,0 +1,20 @@
+package seo.dale.algorithm.sort;
+
+/**
+ * BubleSorter1 보다 비효율적...
+ */
+public class BubbleSorter2 {
+
+	public static void sort(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - 1 -i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = tmp;
+				}
+			}
+		}
+	}
+	
+}
