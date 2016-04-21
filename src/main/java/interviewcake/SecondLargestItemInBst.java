97d3c7a8eb93d3736c3af17f8894c5fe7b35a8bc
서대영/Store기술개발팀/SKP <dale.seo@sk.com>
2016-04-21 14:22:15 +0900

    SecondLargestItemInBst completed

--- src/main/java/interviewcake/SecondLargestItemInBst.java
+++ src/main/java/interviewcake/SecondLargestItemInBst.java
@@ -2,6 +2,8 @@ package interviewcake;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * https://www.interviewcake.com/question/java/second-largest-item-in-bst?utm_source=weekly_email
  * @author 서대영/Store기술개발팀/SKP
@@ -21,6 +23,103 @@ public class SecondLargestItemInBst {
 		node14.insertLeft(13);
 
 		print(root8);
+
+		BinaryTreeNode largest = findLargest(root8);
+		assertEquals(14, largest.value);
+
+		BinaryTreeNode secondLargest = findSecondLargest(root8);
+		assertEquals(13, secondLargest.value);
+
+		BinaryTreeNode secondLargest2 = findSecondLargest2(root8);
+		assertEquals(13, secondLargest2.value);
+	}
+
+	@Test
+	public void test2() {
+		BinaryTreeNode root5 = new BinaryTreeNode(5);
+
+		BinaryTreeNode node3 = root5.insertLeft(3);
+		BinaryTreeNode node8 = root5.insertRight(8);
+
+		node3.insertLeft(1);
+		node3.insertRight(4);
+
+		node8.insertLeft(7);
+		BinaryTreeNode node12 = node8.insertRight(12);
+
+		BinaryTreeNode node10 = node12.insertLeft(10);
+		node10.insertLeft(9);
+		node10.insertRight(11);
+
+		print(root5);
+
+		BinaryTreeNode largest = findLargest(root5);
+		assertEquals(12, largest.value);
+
+		BinaryTreeNode secondLargest = findSecondLargest(root5);
+		assertEquals(11, secondLargest.value);
+
+		BinaryTreeNode secondLargest2 = findSecondLargest2(root5);
+		assertEquals(11, secondLargest2.value);
+	}
+
+	/**
+	 * parent 를 유지하는 방법
+	 */
+	public BinaryTreeNode findSecondLargest(BinaryTreeNode node) {
+		if (node == null || node.left == null || node.right == null) {
+			throw new IllegalArgumentException("Tree must have at least 2 nodes");
+		}
+
+		BinaryTreeNode parent = null;
+		while (node.right != null) {
+			parent = node;
+			node = node.right;
+		}
+
+		if (node.left != null) {
+			return findLargest(node.left);
+		} else {
+			return parent;
+		}
+	}
+
+	/**
+	 * 자식의 자식까지 체크하는 방법
+	 */
+	public BinaryTreeNode findSecondLargest2(BinaryTreeNode node) {
+		if (node == null) {
+			throw new IllegalArgumentException("Tree must have at least 1 node");
+		}
+
+		while (true) {
+			if (node.right == null) {
+				if (node.left == null) {
+					throw new IllegalArgumentException("Tree must have at least 2 nodes");
+				} else {
+					return findLargest(node.left);
+				}
+			} else {
+				if (node.right.left == null && node.right.right == null) {
+					return node;
+				} else {
+					node = node.right;
+				}
+			}
+		}
+	}
+
+	// The rightmost node has the largest value.
+	public BinaryTreeNode findLargest(BinaryTreeNode node) {
+		if (node == null) {
+			throw new IllegalArgumentException("Tree must have at least 1 node");
+		}
+
+		while (node.right != null) {
+			node = node.right;
+		}
+
+		return node;
 	}
 
 	public void print(BinaryTreeNode node) {
