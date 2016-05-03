1ed776133e97b983e97b0e4fb15b3374795d70c6
Dale Seo <dale.seo@gmail.com>
2016-05-03 23:26:42 +0900

    DRAWECT

--- src/main/java/algospot/DRAWRECT/Main.java
+++ src/main/java/algospot/DRAWRECT/Main.java
@@ -3,7 +3,9 @@ package algospot.DRAWRECT;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
@@ -11,18 +13,35 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int numOfTests = scanner.nextInt();
         while (numOfTests --> 0) {
-            int[] xArr = new int[3];
-            int[] yArr = new int[3];
+            int[][] position = new int[3][3];
             for (int i = 0; i < 3; i++) {
-                xArr[i] = scanner.nextInt();
-                yArr[i] = scanner.nextInt();
+                position[i][0] = scanner.nextInt();
+	            position[i][1] = scanner.nextInt();
             }
-            drawrect(xArr, yArr);
+            drawrect(position);
         }
     }
 
-    public static void drawrect(int[] xArr, int[] yArr) {
-        System.out.println("- drawrect");
+    public static void drawrect(int[][] position) {
+        Set<Integer> xSet = new HashSet<>();
+	    Set<Integer> ySet = new HashSet<>();
+
+        for (int i = 0; i < 3; i++) {
+	        int posX = position[i][0];
+            if (xSet.contains(posX)) {
+                xSet.remove(posX);
+            } else {
+	            xSet.add(posX);
+            }
+
+	        int posY = position[i][1];
+	        if (ySet.contains(posY)) {
+		        ySet.remove(posY);
+	        } else {
+		        ySet.add(posY);
+	        }
+        }
+	    System.out.println(xSet.toArray()[0] + " " + ySet.toArray()[0]);
     }
 
     @Test
@@ -37,13 +56,12 @@ public class Main {
         Scanner scanner = new Scanner(new ByteArrayInputStream(input.getBytes()));
         int numOfTests = scanner.nextInt();
         while (numOfTests --> 0) {
-            int[] xArr = new int[3];
-            int[] yArr = new int[3];
+            int[][] position = new int[3][3];
             for (int i = 0; i < 3; i++) {
-                xArr[i] = scanner.nextInt();
-                yArr[i] = scanner.nextInt();
+                position[i][0] = scanner.nextInt();
+                position[i][1] = scanner.nextInt();
             }
-            drawrect(xArr, yArr);
+            drawrect(position);
         }
     }
 
