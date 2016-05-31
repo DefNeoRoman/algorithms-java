5e2f6c1bfd02e7d701a8bc826b50765b52637da6
서대영(DAEYOUNG SEO)/Onestore/SKP <dale.seo@sk.com>
2016-05-31 16:39:52 +0900

    'CONVERT'

---
+++ src/main/java/algospot/CONVERT/Main.java
@@ -0,0 +1,88 @@
+/**
+ * https://www.algospot.com/judge/problem/read/CONVERT
+ */
+package algospot.CONVERT;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in);) {
+			int count = scanner.nextInt();
+			for (int i = 0; i < count; i ++) {
+				double number = scanner.nextDouble();
+				String unitStr = scanner.next();
+				Measurement.Unit unit = Measurement.Unit.valueOf(unitStr);
+				Measurement source = new Measurement(number, unit);
+				Measurement target = convert(source);
+				System.out.printf("%d %s%n", i + 1, target);
+			}
+		}
+	}
+	
+	public static Measurement convert(Measurement source) {
+		double number = -1;
+		Measurement.Unit unit = null;
+		
+		switch(source.getUnit()) {
+		case kg:
+			number = source.getNumber() * 2.2046;
+			unit = Measurement.Unit.lb;
+			break;
+		case lb:
+			number = source.getNumber() * 0.4536;
+			unit = Measurement.Unit.kg;
+			break;
+		case l:
+			number = source.getNumber() * 0.2642;
+			unit = Measurement.Unit.g;
+			break;
+		case g:
+			number = source.getNumber() * 3.7854;
+			unit = Measurement.Unit.l;
+			break;
+		}
+				
+		return new Measurement(number, unit);
+	}
+	
+}
+
+class Measurement {
+	
+	enum Unit {
+		kg, lb, l, g
+	}
+
+	private double number;
+	
+	private Unit unit;
+	
+	public Measurement(double number, Unit unit) {
+		this.number = number;
+		this.unit = unit;
+	}
+
+	public double getNumber() {
+		return number;
+	}
+
+	public void setNumber(double number) {
+		this.number = number;
+	}
+
+	public Unit getUnit() {
+		return unit;
+	}
+
+	public void setUnit(Unit unit) {
+		this.unit = unit;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%.4f %s", number, unit);
+	}
+	
+}
---
+++ src/main/java/algospot/CONVERT/MainTest.java
@@ -0,0 +1,32 @@
+package algospot.CONVERT;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MainTest {
+	
+	@Test
+	public void test() {
+		Measurement measurement1 = Main.convert(new Measurement(1, Measurement.Unit.kg));
+		System.out.println(measurement1);
+		assertEquals("2.2046 lb", measurement1.toString());
+		
+		Measurement measurement2 = Main.convert(new Measurement(2, Measurement.Unit.l));
+		System.out.println(measurement2);
+		assertEquals("0.5284 g", measurement2.toString());
+		
+		Measurement measurement3 = Main.convert(new Measurement(7, Measurement.Unit.lb));
+		System.out.println(measurement3);
+		assertEquals("3.1752 kg", measurement3.toString());
+		
+		Measurement measurement4 = Main.convert(new Measurement(3.5, Measurement.Unit.g));
+		System.out.println(measurement4);
+		assertEquals("13.2489 l", measurement4.toString());
+		
+		Measurement measurement5 = Main.convert(new Measurement(0, Measurement.Unit.l));
+		System.out.println(measurement5);
+		assertEquals("0.0000 g", measurement5.toString());
+	}
+
+}
