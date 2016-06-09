da22c6fb90aea85bb68744c79e916ecc656dae64
Dale Seo <dale.seo@gmail.com>
2016-06-09 23:07:13 +0900

    NQUEEN Completed

--- src/main/java/algospot/NQUEEN/MainTest.java
+++ src/main/java/algospot/NQUEEN/MainTest.java
@@ -3,21 +3,26 @@ package algospot.NQUEEN;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class MainTest {
 
 	@Test
 	public void test() {
-		Main.nqueen(4);
+		assertEquals(1, Main.nqueen(1));
+		assertEquals(0, Main.nqueen(2));
+		assertEquals(0, Main.nqueen(3));
+		assertEquals(2, Main.nqueen(4));
+		assertEquals(92, Main.nqueen(8));
 	}
 
 	@Test
 	public void testCanPutQueenIn() throws Exception {
 		int[] lines = {1, 3, -1, -1};
-		assertTrue(Main.canPutQueenIn(2, 0, lines));
-		assertFalse(Main.canPutQueenIn(2, 1, lines));
-		assertFalse(Main.canPutQueenIn(2, 2, lines));
-		assertFalse(Main.canPutQueenIn(2, 3, lines));
+		assertTrue(Main.canPlaceQueenIn(2, 0, lines));
+		assertFalse(Main.canPlaceQueenIn(2, 1, lines));
+		assertFalse(Main.canPlaceQueenIn(2, 2, lines));
+		assertFalse(Main.canPlaceQueenIn(2, 3, lines));
 	}
 }
