d67055b69f12ea0a7558273c00dacfeb0f699161
서대영/Store기술개발팀/SKP <dale.seo@sk.com>
2016-06-09 10:27:05 +0900

    NQUEEN Try3

--- src/main/java/algospot/NQUEEN/Main.java
+++ src/main/java/algospot/NQUEEN/Main.java
@@ -1,31 +1,67 @@
 package algospot.NQUEEN;
 
+import java.util.Arrays;
+
 /**
  * https://www.algospot.com/judge/problem/read/NQUEEN
  * https://en.wikibooks.org/wiki/Algorithm_Implementation/Miscellaneous/N-Queens
  */
 public class Main {
 
+	private static int size;
+
 	public static void main(String[] args) {
 
 	}
 
 	public static int nqueen(int size) {
+		Main.size = size;
 		int count = 0;
-
-		int[] rows = new int[size];
-
-		for (int r = 0; r < size; r++) {
-			count += check(r, rows);
-		}
+		
+		int[] colPoses = new int[size];
+		Arrays.fill(colPoses, -1);
+		
+		count += check(0, colPoses);
 
 		return count;
 	}
 
-	private static int check(int row, int[] rows) {
-		return -1;
+	private static int check(int rowIdx, int[] colPoses) {
+		System.out.println("Call : rowIdx = [" + rowIdx + "], colPoses = [" + colPoses + "]");
+
+		if (rowIdx == size) {
+			return 1;
+		}
+
+		for (int colPos = 0; colPos < size; colPos++) {
+			if (canPutQueenIn(rowIdx, colPos, colPoses)) {
+				colPoses[rowIdx] = colPos; // mark
+				return check(rowIdx + 1, colPoses); // and find on the next row
+			}
+		}
+
+		return 0; // fail to find any colPos
 	}
 
+	public static boolean canPutQueenIn(int row, int col, int[] colPoses) {
+		for (int i = 1; i <= row; i++) {
+			int upperCol = colPoses[row - i];
+			// check if it conflicts upward
+			if (upperCol == col) {
+				return false;
+			}
 
+			// check if it conflicts left-upward
+			if (upperCol == col - i) {
+				return false;
+			}
+
+			// check if it conflicts right-upward
+			if (upperCol == col + i) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
--- src/main/java/algospot/NQUEEN/MainTest.java
+++ src/main/java/algospot/NQUEEN/MainTest.java
@@ -2,11 +2,22 @@ package algospot.NQUEEN;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 public class MainTest {
 
 	@Test
 	public void test() {
-		Main.nqueen(3);
+		Main.nqueen(4);
 	}
 
+	@Test
+	public void testCanPutQueenIn() throws Exception {
+		int[] lines = {1, 3, -1, -1};
+		assertTrue(Main.canPutQueenIn(2, 0, lines));
+		assertFalse(Main.canPutQueenIn(2, 1, lines));
+		assertFalse(Main.canPutQueenIn(2, 2, lines));
+		assertFalse(Main.canPutQueenIn(2, 3, lines));
+	}
 }
