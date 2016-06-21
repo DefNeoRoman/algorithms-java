1c55a1cec5cc2fb22bab6aa0832c56e810d06904
서대영/Store기술개발팀/SKP <dale.seo@sk.com>
2016-06-21 09:17:05 +0900

    LECTURE solved

--- src/main/java/algospot/LECTURE/Main.java
+++ src/main/java/algospot/LECTURE/Main.java
@@ -1,29 +1,39 @@
+/**
+ * https://www.algospot.com/judge/problem/read/LECTURE
+ */
 package algospot.LECTURE;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+		int count = scanner.nextInt();
+		while (count-- > 0) {
+			System.out.println(sort(scanner.next()));
+		}
+		scanner.close();
 	}
 
 	public static String sort(String text) {
-		List<String> list = new ArrayList<>();
+		List<String> tokens = new ArrayList<>();
 
 		int i = 2;
 		while (i <= text.length()) {
 			String sub = text.substring(i - 2, i);
-			list.add(sub);
+			tokens.add(sub);
 			i += 2;
 		}
 
-		System.out.println(list);
+		Collections.sort(tokens);
 
-		Collections.sort(list);
-		return null;
+		StringBuilder builder = new StringBuilder();
+		tokens.forEach(builder::append);
+		return builder.toString();
 	}
 
 }
