fbf720956c77e09d19a26d72b7ff8d663ce43a94
서대영/Store기술개발팀/SKP <dale.seo@sk.com>
2016-06-29 10:14:19 +0900

    lazySsorting continued

---
+++ src/main/java/hackerrank/contest/weekOfCode21/lazySsorting/Solution.java
@@ -0,0 +1,41 @@
+/**
+ * https://www.hackerrank.com/contests/w21/challenges/lazy-sorting
+ * 확률을 구할 때 중복 원소가 있는 것도 고려해야 할 것 같음
+ */
+package hackerrank.contest.weekOfCode21.lazySsorting;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int cnt = scan.nextInt();
+		solve(cnt);
+	}
+
+	public static void solve(int length) {
+		double sum = 0;
+		int minute = 1;
+		double probability = calculateProbability(length);
+		double antiProbability = 1;
+		while (antiProbability * 1000000000 > 1) {
+			double cost = minute * antiProbability * probability;
+			sum += minute * antiProbability * probability;
+			// System.out.printf("plus %2d * %.8f => %.8f%n", minute, cost, sum);
+			minute++;
+			antiProbability *= 1 - probability;
+		}
+		// System.out.println(sum);
+		System.out.printf("%.6f", sum);
+	}
+
+	private static double calculateProbability(int length) {
+		int factorial = length;
+		while(--length > 0) {
+			factorial *= length;
+		}
+		return 1.0 / factorial;
+	}
+
+}
---
+++ src/main/java/hackerrank/contest/weekOfCode21/lazySsorting/SolutionTest.java
@@ -0,0 +1,25 @@
+package hackerrank.contest.weekOfCode21.lazySsorting;
+
+import org.junit.Test;
+
+/**
+ * @author 서대영(DAEYOUNG SEO)/Onestore/SKP
+ */
+public class SolutionTest {
+
+	@Test
+	public void testSolve() throws Exception {
+		Solution.solve(1);
+	}
+
+	@Test
+	public void testSolve2() throws Exception {
+		Solution.solve(2);
+	}
+
+	@Test
+	public void testSolve3() throws Exception {
+		Solution.solve(3);
+	}
+
+}
