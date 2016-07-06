b9233a533d288b800e1482d99299d798c1dfa4bb
서대영/Store기술개발팀/SKP <dale.seo@sk.com>
2016-07-06 10:26:50 +0900

    SnakesAndLadders continued

--- src/main/java/hackerrank/algorithms/graph/SnakesAndLadders/Solution.java
+++ src/main/java/hackerrank/algorithms/graph/SnakesAndLadders/Solution.java
@@ -1,10 +1,97 @@
 /**
  * https://www.hackerrank.com/challenges/the-quickest-way-up
+ * https://www.youtube.com/watch?v=19tY6Y90TP0
  */
 package hackerrank.algorithms.graph.SnakesAndLadders;
+
+import java.util.*;
 
 /**
  * @author 서대영(DAEYOUNG SEO)/Onestore/SKP
  */
 public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int cnt = scan.nextInt();
+
+		while (cnt-- > 0) {
+			int numOfLadders = scan.nextInt();
+			Map<Integer, Integer> ladderMap = new HashMap<>();
+			while (numOfLadders-- > 0) {
+				ladderMap.put(scan.nextInt() - 1, scan.nextInt() - 1);
+			}
+
+			int numOfSnakes = scan.nextInt();
+			Map<Integer, Integer> snakeMap = new HashMap<>();
+			while (numOfSnakes-- > 0) {
+				snakeMap.put(scan.nextInt() - 1, scan.nextInt() - 1);
+			}
+
+			System.out.println(solve(100, ladderMap, snakeMap));
+		}
+
+		scan.close();
+	}
+
+	public static int solve(int size, Map<Integer, Integer> ladderMap, Map<Integer, Integer> snakeMap) {
+		boolean[] visited = new boolean[size];
+
+		Queue<Entry> queue = new LinkedList<>();
+
+		queue.add(new Entry(0, 0));
+
+		while (!queue.isEmpty()) {
+			Entry current = queue.remove();
+
+			if (current.vertex == size - 1) {
+				return current.distance;
+			}
+
+			int nextDistance = current.distance + 1;
+
+			// roll dices
+			for (int i = 0; i < 6; i++) {
+				int nextVertex = current.vertex + 1 + i;
+
+				if (nextVertex > size - 1) {
+					break;
+				}
+
+				if (visited[nextVertex]) {
+					continue;
+				}
+
+				if (ladderMap.containsKey(nextVertex)) {
+					nextVertex = ladderMap.get(nextVertex);
+				} else if (snakeMap.containsKey(nextVertex)) {
+					nextVertex = snakeMap.get(nextVertex);
+				}
+
+				Entry next = new Entry(nextVertex, nextDistance);
+				queue.add(next);
+				visited[nextDistance] = true;
+			}
+			System.out.println("--------------------------------------------------------------------------------------");
+			System.out.println(queue);
+		}
+
+		return -1;
+	}
+
+	static class Entry {
+		int vertex;
+		int distance;
+
+		public Entry(int vertex, int distance) {
+			this.vertex = vertex;
+			this.distance = distance;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("%d(%d)", vertex, distance);
+		}
+	}
+
 }
