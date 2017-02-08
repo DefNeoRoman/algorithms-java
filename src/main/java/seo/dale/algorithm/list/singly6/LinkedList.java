6be091948a55218e26cb3182407b3495e9d02e0e
Dale Seo <dale.seo@gmail.com>
2017-02-08 09:52:44 +0900

    LinkedList (https://youtu.be/njTh_OwMljA)

---
+++ src/main/java/seo/dale/algorithm/list/singly6/LinkedList.java
@@ -0,0 +1,72 @@
+package seo.dale.algorithm.list.singly6;
+
+/**
+ * https://youtu.be/njTh_OwMljA
+ */
+public class LinkedList<T> {
+
+	Node<T> head;
+
+	public void append(T data) {
+		if (head == null) {
+			head = new Node<>(data);
+			return;
+		}
+		Node<T> current = head;
+		while (current.next != null) {
+			current = current.next;
+		}
+		current.next = new Node<>(data);
+	}
+
+	public void prepend(T data) {
+		Node<T> newHead = new Node<>(data);
+		newHead.next = head;
+		head = newHead;
+	}
+
+	public void deleteWithValue(T data) {
+		if (head == null) {
+			return;
+		}
+		if (head.data == data) {
+			head = head.next;
+			return;
+		}
+		Node<T> current = head;
+		while (current.next != null) {
+			if (current.next.data == data) {
+				current.next = current.next.next;
+				return;
+			}
+			current = current.next;
+		}
+	}
+
+	public static class Node<T> {
+
+		T data;
+		Node next;
+
+		public Node(T data) {
+			this.data = data;
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		Node<T> node = head;
+		while (node != null) {
+			builder.append(node.data);
+			builder.append("->");
+			node = node.next;
+		}
+		if (builder.length() > 2) {
+			builder.setLength(builder.length() - 2);
+		}
+		return builder.toString();
+	}
+
+}
---
+++ src/main/java/seo/dale/algorithm/list/singly6/LinkedListTest.java
@@ -0,0 +1,49 @@
+package seo.dale.algorithm.list.singly6;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LinkedListTest {
+
+	private LinkedList<Character> linkedList;
+
+	@Before
+	public void setUp() throws Exception {
+		linkedList = new LinkedList<>();
+	}
+
+	@Test
+	public void testPrepend() throws Exception {
+		linkedList.prepend('A');
+		assertEquals("A", linkedList.toString());
+		linkedList.prepend('B');
+		assertEquals("B->A", linkedList.toString());
+		linkedList.prepend('C');
+		assertEquals("C->B->A", linkedList.toString());
+	}
+
+	@Test
+	public void testappend() throws Exception {
+		linkedList.append('A');
+		assertEquals("A", linkedList.toString());
+		linkedList.append('B');
+		assertEquals("A->B", linkedList.toString());
+		linkedList.append('C');
+		assertEquals("A->B->C", linkedList.toString());
+	}
+
+	@Test
+	public void testDelete() throws Exception {
+		linkedList.prepend('C');
+		linkedList.prepend('B');
+		linkedList.prepend('A');
+
+		linkedList.deleteWithValue('B');
+		assertEquals("A->C", linkedList.toString());
+		linkedList.deleteWithValue('A');
+		assertEquals("C", linkedList.toString());
+	}
+
+}
