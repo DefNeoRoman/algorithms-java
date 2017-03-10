3ef0dc4ad9eda7fcc9553348f00788dbbaea316c
Dale Seo <dale.seo@gmail.com>
2017-03-10 09:40:07 +0900

    https://codility.com/tasks/tree_visibility

---
+++ src/main/java/codility/tree_visibility/Solution.java
@@ -0,0 +1,31 @@
+package codility.tree_visibility;
+
+/**
+ * https://codility.com/tasks/tree_visibility/
+ * https://codility.com/c/run/tryDR7AH7-UKU
+ * https://codility.com/tickets/tryDR7AH7-UKU/
+ */
+public class Solution {
+	public int solution(Tree T) {
+		return countVisibleNodes(T, Integer.MIN_VALUE);
+	}
+
+	private int countVisibleNodes(Tree node, int max) {
+		if (node == null) {
+			return 0;
+		}
+		int count = 0;
+		if (node.x >= max) {
+			max = node.x;
+			count += 1;
+		}
+		count += countVisibleNodes(node.l, max) + countVisibleNodes(node.r, max);
+		return count;
+	}
+}
+
+class Tree {
+	public int x;
+	public Tree l;
+	public Tree r;
+}
