803d0bf92a6e5e5c924fb34462890aeaef61407e
Dale Seo <dale.seo@gmail.com>
2017-05-02 10:51:23 +0900

    https://codility.com/tasks/pow11/

--- src/main/java/codility/pow11/Solution.java
+++ src/main/java/codility/pow11/Solution.java
@@ -1,6 +1,6 @@
 package codility.pow11;
 
-import java.math.BigDecimal;
+import java.math.BigInteger;
 
 /**
  * https://codility.com/tasks/pow11/
@@ -10,14 +10,14 @@ public class Solution {
 
 	public static void main(String[] args) {
 		Solution s = new Solution();
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 100; i++) {
 			System.out.println(i + " : " + s.solution(i));
 		}
 	}
 
 	public int solution(int N) {
-		BigDecimal eleven = new BigDecimal("11");
-		BigDecimal power = new BigDecimal("1");
+		BigInteger eleven = new BigInteger("11");
+		BigInteger power = new BigInteger("1");
 		while (N-- > 0) {
 			power = power.multiply(eleven);
 		}
